0947f92fd6cb0b4b75b4d919f66036819d20492b
Iyiola-am <adekunle3317@gmail.com>
2021-02-23 14:26:36 +0100

    Refactored auth middlewarwe

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 import jwt from '../lib/jwt'
 import {AuthenticationError} from '../lib/errors'
-import UserModel, {User} from '../models/user'
+import UserModel from '../models/user'
 import { NextFunction, Request, Response } from 'express';
 
 export default async function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
@@ -21,7 +21,7 @@ export default async function AuthMiddleware(req: Request, res: Response, next: 
 
 
     try {
-        let user = (await UserModel.findOne({id}).exec()) as User|null;
+        let user = await UserModel.findOne({id}).exec();
 
         if (user == null) throw new Error('User is not Authenticated');
         else if (await user.hasToken(token) != 0) throw new Error('User is not Authenticated');
