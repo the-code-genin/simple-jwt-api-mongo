a6a1b1b6c3131be38c403d7e9e293799ead1d8d9
Iyiola-am <adekunle3317@gmail.com>
2021-04-19 16:49:17 +0100

    Added auth token model

---
+++ models/auth-token.ts
@@ -0,0 +1,36 @@
+import { Document, model, Schema } from "mongoose";
+
+
+// Document
+export interface AuthToken extends Document {
+    token?: string,
+    user_id?: string,
+    created_at?: Date,
+    updated_at?: Date
+}
+
+
+// Schema
+let schema = new Schema<AuthToken>({
+    token: {
+        type: String,
+        index: true,
+        required: true
+    },
+    user_id: {
+        type: String,
+        index: true,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+}, {collection: 'auth_tokens'});
+
+
+export default model('AuthToken', schema);
