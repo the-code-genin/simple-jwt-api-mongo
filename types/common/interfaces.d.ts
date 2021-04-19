57258fcb8a90c2e0e0f2d3f58b62315531e2fe2f
Iyiola-am <adekunle3317@gmail.com>
2021-04-19 17:05:30 +0100

    Refactored interfaces

--- types/common/interfaces.d.ts
+++ types/common/interfaces.d.ts
@@ -12,6 +12,14 @@ interface ApiResponse {
     }
 }
 
-interface Serializable {
-    toJSON(): {[key: string]: any};
+interface ResponsePayload<T> {
+    data?: T|T[],
+    total?: number,
+    per_page?: number,
+    current_page?: number,
+    prev_page?: number|null,
+    next_page?: number|null,
+    from?: number,
+    to?: number,
+    [key: string]: any
 }
