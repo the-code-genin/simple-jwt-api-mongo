29c4d2e74426accbe4513c4eedf21e28a708f57f
Mohammed Adekunle <adekunle3317@gmail.com>
2021-06-09 18:17:44 +0100

    Refactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -6,7 +6,7 @@ import { NextFunction, Request, Response } from 'express';
 export default async function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
     let header = req.get('Authorization') as string;
     if (!/^Bearer (.+)$/i.test(header)) { // Bearer token is not present
-        res.json(AuthenticationError('User is not Authenticated'));
+        res.status(401).json(AuthenticationError('User is not Authenticated'));
         return;
     }
 
@@ -15,7 +15,7 @@ export default async function AuthMiddleware(req: Request, res: Response, next: 
     let token = (/^Bearer (.+)$/i.exec(header) as string[])[1].trim();
     let id = jwt.verifyAccessToken(token);
     if (!id) { // Invalid Bearer token
-        res.json(AuthenticationError('User is not Authenticated'));
+        res.status(401).json(AuthenticationError('User is not Authenticated'));
         return;
     }
 
@@ -30,6 +30,6 @@ export default async function AuthMiddleware(req: Request, res: Response, next: 
         req.app.set('authUser', user);
         next();
     } catch(e) {
-        res.json(AuthenticationError(e.message));
+        res.status(401).json(AuthenticationError((e as Error).message));
     }
 }
