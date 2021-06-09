784be73a5e23e06711e614c231084855b25f5622
Mohammed Adekunle <adekunle3317@gmail.com>
2021-06-09 18:22:06 +0100

    Updated errors

--- lib/errors.ts
+++ lib/errors.ts
@@ -11,16 +11,20 @@ export function ApplicationError(code: number, type: string, message: string): A
     }
 }
 
-export function NotFoundError(message: string = 'The resource you were looking for was not found on this server.') {
-    return ApplicationError(404, 'NotFoundError', message);
+export function InvalidFormDataError(message: string = 'Invalid form data!') {
+    return ApplicationError(400, 'InvalidFormData', message);
 }
 
 export function AuthenticationError(message: string = 'User is not Authenticated!') {
     return ApplicationError(401, 'AuthenticationError', message);
 }
 
-export function InvalidFormDataError(message: string = 'Invalid form data!') {
-    return ApplicationError(401, 'InvalidFormData', message);
+export function NotFoundError(message: string = 'The resource you were looking for was not found on this server.') {
+    return ApplicationError(404, 'NotFoundError', message);
+}
+
+export function ConflictError(message: string = 'The resource you are trying to create already exists!') {
+    return ApplicationError(409, 'Conflict', message);
 }
 
 export function ServerError(message: string = 'Server Error!') {
