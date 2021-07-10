6482fbcb5817b1133196fec14878d1636995df8c
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:44:49 +0100

    Refactored JWT class

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,9 +1,8 @@
 import jwt from 'jsonwebtoken'
-import {User} from '../models/user';
+import { User } from '../models/user';
 
 export interface JWTData {
     iss: string,
-    exp: number,
     sub: string
 };
 
@@ -11,27 +10,12 @@ export default class JWT {
     /**
      * Extract user id from a valid access token.
      */
-    static verifyAccessToken(token: string): string|null {
+    static verifyAccessToken(token: string): string | null {
         try {
             let payload = jwt.verify(token, String(process.env.APP_KEY)) as JWTData;
             return payload.sub;
-        } catch(e) {
+        } catch (e) {
             return null;
-        }
-    }
-
-    /**
-     * Extract user id from a valid or expired access token.
-     */
-    static verifyExpiredAccessToken(token: string): string|null {
-        try {
-            let payload = jwt.verify(token, String(process.env.APP_KEY)) as JWTData;
-            return payload.sub;
-        } catch(e) {
-            if (e instanceof jwt.TokenExpiredError) {
-                let payload = jwt.decode(token) as JWTData;
-                return payload.sub;
-            } else return null;
         }
     }
 
@@ -41,7 +25,6 @@ export default class JWT {
     static generateAccessToken(user: User): string {
         let data: JWTData = {
             iss: String(process.env.APP_URL),
-            exp: (new Date).getTime() + Number(process.env.JWT_TTI),
             sub: user._id
         };
 
