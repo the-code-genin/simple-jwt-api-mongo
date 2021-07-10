270a805d458e428aa58fa0dde06d4f8b98b2df5e
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-10 17:45:28 +0100

    Refactored auth controller

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
 import { AuthenticationError, ServerError } from "../lib/errors";
 import JWT from "../lib/jwt";
-import UserModel, {User} from "../models/user";
+import UserModel, { User } from "../models/user";
 import AuthToken from "../models/auth-token";
 
 export default class AuthController {
@@ -11,7 +11,7 @@ export default class AuthController {
      */
     static async login(req: Request, res: Response) {
         try {
-            let user = await UserModel.findOne({email: req.body.email}).exec();
+            let user = await UserModel.findOne({ email: req.body.email }).exec();
 
             if (user == null) throw new Error("Email and password combination do not match a user in our system.");
             else if (!await bcrypt.compare(req.body.password, String(user.password))) throw new Error("Email and password combination do not match a user in our system.");
@@ -21,11 +21,10 @@ export default class AuthController {
                 payload: {
                     data: user.toJSON(),
                     access_token: JWT.generateAccessToken(user),
-                    token_type: 'bearer',
-                    expires_in: process.env.JWT_TTI
+                    token_type: 'bearer'
                 }
             });
-        } catch(e) {
+        } catch (e) {
             res.status(401).json(AuthenticationError((e as Error).message));
         }
     }
@@ -44,8 +43,7 @@ export default class AuthController {
             payload: {
                 data: user.toJSON(),
                 access_token: JWT.generateAccessToken(user),
-                token_type: 'bearer',
-                expires_in: process.env.JWT_TTI
+                token_type: 'bearer'
             }
         });
     }
@@ -72,7 +70,7 @@ export default class AuthController {
                     data: {}
                 }
             });
-        } catch(e) {
+        } catch (e) {
             res.status(500).json(ServerError((e as Error).message));
         }
     }
@@ -87,52 +85,5 @@ export default class AuthController {
                 data: (req.app.get('authUser') as User).toJSON()
             }
         });
-    }
-
-    /**
-     * Refresh a user's JWT auth token and invalidate the previous token.
-     */
-    static async refresh(req: Request, res: Response) {
-        let header = String(req.get('Authorization'));
-        if (!/^Bearer (.+)$/i.test(header)) { // If bearer token is not present.
-            res.json(AuthenticationError('User is not Authenticated'));
-            return;
-        }
-
-
-        // Extract user id from bearer token
-        let matches = /^Bearer (.+)$/i.exec(header) as RegExpExecArray;
-        let token = matches[1].trim();
-        let id = JWT.verifyExpiredAccessToken(token);
-        if (!id) { // Invalid bearer token.
-            res.json(AuthenticationError('User is not Authenticated'));
-            return;
-        }
-
-
-        try {
-            let user = await UserModel.findOne({_id: id}).exec();
-            if (user == null) throw new Error('User is not Authenticated');
-
-
-            // Invalidate the previous auth token.
-            await AuthToken.create({
-                token,
-                user_id: user._id
-            });
-
-
-            // Generate and return new auth token.
-            res.status(200).json({
-                success: true,
-                payload: {
-                    access_token: JWT.generateAccessToken(user),
-                    token_type: 'bearer',
-                    expires_in: process.env.JWT_TTI
-                }
-            });
-        } catch(e) {
-            res.status(401).json(AuthenticationError((e as Error).message));
-        }
     }
 }
