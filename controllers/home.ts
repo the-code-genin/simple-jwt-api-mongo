391dc940669c81599a0654232ce5468fce74a9ca
Mohammed Adekunle <adekunle3317@gmail.com>
2021-08-05 12:16:12 +0100

    Updated view rendering code

--- controllers/home.ts
+++ controllers/home.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 
 export default class HomeController {
     static index(req: Request, res: Response) {
-        res.status(200).render('index');
+        res.status(200).render('index.twig');
     }
 }
