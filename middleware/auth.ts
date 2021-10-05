bdf8c828d136cd069c116234abc03510165b0159
Mohammed Adekunle <adekunle3317@gmail.com>
2021-10-05 13:08:07 +0100

    Refactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 import jwt from '../lib/jwt'
-import {AuthenticationError} from '../lib/errors'
-import UserModel from '../models/user'
+import { AuthenticationError } from '../lib/errors'
+import UserModel, { User } from '../models/user'
 import { NextFunction, Request, Response } from 'express';
 
 export default async function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
@@ -10,7 +10,6 @@ export default async function AuthMiddleware(req: Request, res: Response, next: 
         return;
     }
 
-
     // Extract user ID from bearer token
     let token = (/^Bearer (.+)$/i.exec(header) as string[])[1].trim();
     let id = jwt.verifyAccessToken(token);
@@ -19,17 +18,19 @@ export default async function AuthMiddleware(req: Request, res: Response, next: 
         return;
     }
 
-
+    // Get the user record
+    let user: User | undefined;
     try {
-        let user = await UserModel.findOne({_id: id}).exec();
+        user = await UserModel.findOne({ _id: id }).exec();
 
         if (user == null) throw new Error('User is not Authenticated');
         else if (await user.hasToken(token)) throw new Error('User is not Authenticated');
         else if (user.status != 'active') throw new Error('User is banned from using the platform');
-
-        req.app.set('authUser', user);
-        next();
-    } catch(e) {
+    } catch (e) {
         res.status(401).json(AuthenticationError((e as Error).message));
+        return;
     }
+
+    req.app.set('authUser', user);
+    next();
 }
