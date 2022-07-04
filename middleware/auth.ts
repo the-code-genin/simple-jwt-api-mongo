0ace3f246ea5631995102cd821dd587046bef600
the-code-genin <adekunle3317@gmail.com>
2022-07-04 20:19:22 +0100

    Refactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,36 +1,36 @@
-import jwt from '../lib/jwt'
-import { AuthenticationError } from '../lib/errors'
-import UserModel, { User } from '../models/user'
+import jwt from '../lib/helpers/jwt'
+import { AuthenticationError } from '../lib/responses/errors'
 import { NextFunction, Request, Response } from 'express';
+import Users from '../database/repositories/users';
+import { User } from '../database/models/user';
 
 export default async function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
-    let header = String(req.get('Authorization'));
+    let header = req.get('Authorization') as string;
     if (!/^Bearer (.+)$/i.test(header)) { // Bearer token is not present
-        res.status(401).json(AuthenticationError('User is not Authenticated'));
-        return;
+        return AuthenticationError(res, "Bad token.");
     }
 
     // Extract user ID from bearer token
     let token = (/^Bearer (.+)$/i.exec(header) as string[])[1].trim();
     let id = jwt.verifyAccessToken(token);
     if (!id) { // Invalid Bearer token
-        res.status(401).json(AuthenticationError('User is not Authenticated'));
-        return;
+        return AuthenticationError(res, "Bad/Expired token.");
     }
 
-    // Get the user record
-    let user: User | undefined;
+    // Get the user
+    let user: User | null;
     try {
-        user = await UserModel.findOne({ _id: id }).exec();
-
-        if (user == null) throw new Error('User is not Authenticated');
-        else if (await user.hasToken(token)) throw new Error('User is not Authenticated');
-        else if (user.status != 'active') throw new Error('User is banned from using the platform');
+        user = await Users.getUserByID(id);
+        if (user == null) {
+            throw new Error('User is not Authenticated.');
+        } else if (await Users.checkUserHasAuthToken(user.id, token)) {
+            throw new Error('Expired auth token.');
+        }
     } catch (e) {
-        res.status(401).json(AuthenticationError((e as Error).message));
-        return;
+        return AuthenticationError(res, (e as Error).message);
     }
 
+    // Pass the user object to the request and execute subsequent requests
     req.app.set('authUser', user);
     next();
 }
