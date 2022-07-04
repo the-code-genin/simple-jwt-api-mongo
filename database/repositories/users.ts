f38349b220c33812d11fb798d62da1dd2a521fe3
the-code-genin <adekunle3317@gmail.com>
2022-07-04 20:33:22 +0100

    Refactored database layer

---
+++ database/repositories/users.ts
@@ -0,0 +1,71 @@
+import UserModel, { User } from "../models/user";
+import AuthTokenModel from "../models/auth-token";
+
+export default class Users {
+    static getUsersWithEmailCount(email: string) {
+        return new Promise<number>((resolve, reject) => {
+            UserModel.countDocuments({ email }, (err, count) => {
+                if (err != null) reject(err);
+                else resolve(count);
+            });
+        });
+    }
+
+    static getUserByEmail(email: string) {
+        return new Promise<User | null>((resolve, reject) => {
+            UserModel.findOne({ email }, null, null, (err, user) => {
+                if (err != null) reject(err);
+                else resolve(user);
+            });
+        });
+    }
+
+    static getUserByID(_id: string) {
+        return new Promise<User | null>((resolve, reject) => {
+            UserModel.findOne({ _id }, null, null, (err, user) => {
+                if (err != null) reject(err);
+                else resolve(user);
+            });
+        });
+    }
+
+    static checkUserHasAuthToken(userId: number, token: string) {
+        return new Promise<boolean>((resolve, reject) => {
+            AuthTokenModel.countDocuments({ user_id: userId, token }, (err, count) => {
+                if (err != null) reject(err);
+                else resolve(count != 0);
+            });
+        });
+    }
+
+    static addUserAuthToken(userId: number, token: string) {
+        return new Promise<null>((resolve, reject) => {
+            AuthTokenModel.create({ user_id: userId, token }, (err, doc) => {
+                if (err != null) reject(err);
+                else resolve(null);
+            });
+        });
+    }
+
+    static insert(user: Partial<User>) {
+        return new Promise<User>((resolve, reject) => {
+            UserModel.create(user, (err, doc) => {
+                if (err != null) reject(err);
+                else resolve(doc);
+            });
+        });
+    }
+
+    static updateById(userId: number, user: Partial<User>) {
+        return new Promise<null>((resolve, reject) => {
+            UserModel.updateOne({_id: userId}, user, null, (err, res) => {
+                if (err != null) reject(err);
+                else resolve(null);
+            });
+        });
+    }
+
+    static toJSON(user: User) {
+        return user.toJSON();
+    }
+}
