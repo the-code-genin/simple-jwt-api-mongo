565474998e82fdcf34a21254c0e84e5c8a021088
the-code-genin <adekunle3317@gmail.com>
2022-09-08 02:07:10 +0100

    Implemented logging

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ import corsMiddleware from "cors";
 import routes from "./routes";
 import config from "./config";
 import {resolve as resolvePath} from "path";
+import Logger from "./logger";
 
 process.on("SIGINT", () => process.exit());
 
 (async function () {
     dotenv.config({ path: resolvePath(__dirname, "../.env") });
     const conf = config();
+    Logger.debug("Configured env variables");
 
     // Connnect to db
     await connect(conf.db.uri, {
@@ -21,17 +23,20 @@ process.on("SIGINT", () => process.exit());
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
+    Logger.debug("Connected to DB");
 
     // Create and configure express router
     const app = express();
     app.use(corsMiddleware());
     app.use(express.json());
+    Logger.debug("Configured router");
 
     // Register all app routes.
     routes(app);
+    Logger.debug("Registered app routes");
 
     // Start server.
     app.listen(conf.app.port, () => {
-        console.log(`App running on :${conf.app.port}`);
+        Logger.info(`App running on :${conf.app.port}`);
     });
 })();
